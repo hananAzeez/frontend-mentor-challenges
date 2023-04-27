feb21438dfdbc318ac4f2a6f1b6de9c3beadf5fb
hananAzeez <hanan.azeez112@gmail.com>
2023-04-28 01:31:12 +0530

    input validation

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -2,12 +2,24 @@ const day = document.getElementById("day");
 const month = document.getElementById("month");
 const year = document.getElementById("year");
 
+// LABEL
+const dayLabel = document.getElementById("dayLabel");
+const monthLabel = document.getElementById("monthLabel");
+const yearLabel = document.getElementById("yearLabel");
+
+// ERROR TEXT
+const dayErrorText = document.getElementById("day-error-text");
+const monthErrorText = document.getElementById("month-error-text");
+const yearErrorText = document.getElementById("year-error-text");
+
 // RESULT
 const dayResult = document.getElementById("day-result");
 const monthResult = document.getElementById("month-result");
 const yearResult = document.getElementById("year-result");
 
 const submit = document.getElementById("submit");
+
+console.log(dayLabel.innerText);
 
 const today = new Date();
 const currentDay = today.getDate();
@@ -27,6 +39,9 @@ submit.addEventListener("click", () => {
     dayValue >= 1 && dayValue <= 31 && dayValue !== 0 && dayValue !== "";
   const isValidYear = yearValue >= 1900 && yearValue <= currentYear;
 
+  // DAY CHECK
+  !isValidDay && (dayErrorText.innerHTML = "Must be a valid day");
+  !isValidMonth && (monthErrorText.innerHTML = "Must be a valid month");
   // RESULT
   const resultYear = isValidYear && currentYear - yearValue;
   const resultMonth = isValidMonth && currentMonth - monthValue;
