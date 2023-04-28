c14370dfd37d7ae7cc6e056fe8aa776b61ef0cd5
hananAzeez <hanan.azeez112@gmail.com>
2023-04-28 19:47:24 +0530

    logic

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -26,27 +26,83 @@ const currentDay = today.getDate();
 const currentMonth = today.getMonth() + 1;
 const currentYear = today.getFullYear();
 
+// FUNCTIONS
+const checkMonth = (month) => {
+  const isValid = month >= 1 && month <= 12 && month !== 0 && month !== "";
+  return isValid;
+};
+const checkDay = (day) => {
+  const isValid = day >= 1 && day <= 31 && day !== 0 && day !== "";
+  return isValid;
+};
+const checkYear = (year) => {
+  if (year == "" || year == 0) {
+    const isValid = false;
+    const errorText = "This field is required";
+    return { isValid, errorText };
+  } else if (year > currentYear) {
+    const isValid = false;
+    const errorText = "Must be in the past";
+    return { isValid, errorText };
+  } else if (year <= 1900) {
+    const isValid = false;
+    const errorText = "Must be after 1900";
+    return { isValid, errorText };
+  } else {
+    const isValid =
+      year >= 1900 && year <= currentYear && year !== 0 && year !== "";
+    const errorText = "";
+    return { isValid, errorText };
+  }
+};
+
 submit.addEventListener("click", () => {
   const dayValue = day.value;
   const monthValue = month.value;
   const yearValue = year.value;
-  const isValidMonth =
-    monthValue >= 1 &&
-    monthValue <= 12 &&
-    monthValue !== 0 &&
-    monthValue !== "";
-  const isValidDay =
-    dayValue >= 1 && dayValue <= 31 && dayValue !== 0 && dayValue !== "";
-  const isValidYear = yearValue >= 1900 && yearValue <= currentYear;
+
+  const isValidMonth = checkMonth(monthValue);
+  const isValidDay = checkDay(dayValue);
+  const isValidYear = checkYear(yearValue);
 
   // DAY CHECK
-  !isValidDay && (dayErrorText.innerHTML = "Must be a valid day");
-  !isValidMonth && (monthErrorText.innerHTML = "Must be a valid month");
+  !isValidDay &&
+    ((dayErrorText.innerHTML = "Must be a valid day"),
+    dayErrorText.classList.remove("hidden"));
+
+  !isValidMonth &&
+    ((monthErrorText.innerHTML = "Must be a valid month"),
+    monthErrorText.classList.remove("hidden"));
+
+  !isValidYear &&
+    ((yearErrorText.innerHTML = isValidYear.errorText),
+    yearErrorText.classList.remove("hidden"));
   // RESULT
-  const resultYear = isValidYear && currentYear - yearValue;
-  const resultMonth = isValidMonth && currentMonth - monthValue;
-  const resultDay = isValidDay && currentDay - dayValue;
-  isValidYear && (yearResult.innerHTML = resultYear);
-  isValidMonth && (monthResult.innerHTML = resultMonth);
-  isValidDay && (dayResult.innerHTML = resultDay);
+  let resultYear = isValidYear && currentYear - yearValue;
+  let resultMonth = isValidMonth && currentMonth - monthValue;
+  let resultDay = isValidDay && currentDay - dayValue;
+
+  if (resultMonth < 0) {
+    resultMonth += 12;
+    resultYear -= 1;
+  }
+
+  const isValidForm = isValidDay && isValidMonth && isValidYear.isValid;
+
+  !isValidForm && day.classList.add("error"),
+    month.classList.add("error"),
+    year.classList.add("error");
+
+  isValidForm &&
+    ((yearResult.innerHTML = resultYear),
+    yearErrorText.classList.add("hidden"),
+    year.classList.remove("error"));
+  isValidForm &&
+    ((monthResult.innerHTML = resultMonth),
+    monthErrorText.classList.add("hidden"),
+    month.classList.remove("error"));
+  isValidForm &&
+    ((dayResult.innerHTML = resultDay),
+    dayErrorText.classList.add("hidden"),
+    day.classList.remove("error"));
 });
