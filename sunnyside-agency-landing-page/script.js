20b8e1f910d887722c7f69b05e354a72fabc6fbd
hananAzeez <hanan.azeez112@gmail.com>
2023-09-05 19:08:18 +0530

    scroll event for footer logo

--- sunnyside-agency-landing-page/script.js
+++ sunnyside-agency-landing-page/script.js
@@ -1,12 +1,13 @@
 const menuBtn = document.querySelector(".menu-btn");
 const mobileMenu = document.querySelector(".mobile-nav");
-const logo = document.querySelector(".logo");
+const logo = document.querySelectorAll(".logo");
 
 document.querySelector(".menu-btn").addEventListener("click", () => {
     mobileMenu.classList.toggle("is-active");
 });
 
 logo.addEventListener("click", () => {
+    console.log('clicked')
     window.scrollTo({
         top: 0,
         behavior: "smooth"
