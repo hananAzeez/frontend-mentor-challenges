08f6396090b738a66898f3444fb496b240a7487d
hananAzeez <hanan.azeez112@gmail.com>
2023-10-09 23:15:18 +0530

    code cleanup

--- sunnyside-agency-landing-page/script.js
+++ sunnyside-agency-landing-page/script.js
@@ -5,10 +5,8 @@ const logo = document.querySelectorAll(".logo");
 document.querySelector(".menu-btn").addEventListener("click", () => {
     mobileMenu.classList.toggle("is-active");
 });
-console.log('click')
 
 logo.addEventListener("click", () => {
-    console.log('clicked')
     window.scrollTo({
         top: 0,
         behavior: "smooth"
